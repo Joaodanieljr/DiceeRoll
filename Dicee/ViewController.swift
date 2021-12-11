389598a945ae98b26329883074f2f5b476b9173a
Joao Daniel <joaodanieljr@gmail.com>
2021-12-10 23:17:44 -0300

    study classe 1

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(named: "DiceTwo")
+        diceImageView2.image = UIImage(named: "DiceTwo")
+    }
+    
 }
 
