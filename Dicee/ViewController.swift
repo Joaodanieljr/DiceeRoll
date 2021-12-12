19d2ac5e4f22e9340cfacc75f6a0400b7a122706
Joao Daniel <joaodanieljr@gmail.com>
2021-12-12 15:04:17 -0300

    finish class

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-   
+    
+    let list_dicee =  [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = UIImage(named: "DiceTwo")
-        diceImageView2.image = UIImage(named: "DiceTwo")
+        diceImageView1.image = list_dicee[Int.random(in: 0...5)]
+        diceImageView2.image = list_dicee[Int.random(in: 0...5)]
+        
     }
     
 }
